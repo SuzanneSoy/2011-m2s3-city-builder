8ff03fb150e718249a71a4cfb42f5cd630c75380
Yoann <yoann.b87@voila.fr>
2012-01-06 10:52:10 +0100

    Début de la décomposition des blocks de maison en murs.

--- rules/batiment/batimentquadblock.cpp
+++ rules/batiment/batimentquadblock.cpp
@@ -9,6 +9,17 @@ void BatimentQuadBlock::getBoundingBoxPoints() {
 	addBBPoints(c + Vertex(0,0,height)); // TODO
 }
 
+bool BatimentQuadBlock::split() {
+    Quad me = c;
+    Quad ms = c;
+    Quad mw = c;
+    Quad mn = c;
+
+    me[NW] = me[NE] + ((me[NW] - me[NW]) / me.length(N))*150;
+    //TODO les autres côtés.
+    return false;
+}
+
 void BatimentQuadBlock::triangulation() {
     addGPUOcto(c, c + Vertex(0,0,height), 0xF1, 0xE0, 0xE0);
 }
