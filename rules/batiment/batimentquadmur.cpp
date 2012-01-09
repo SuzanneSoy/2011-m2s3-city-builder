a8e6da67577ca16a6fd0efbc1ba35fcd201befe1
Yoann <yoann.b87@voila.fr>
2012-01-09 20:35:41 +0100

    Début de la division des murs pour placer un fenêtre.

--- rules/batiment/batimentquadmur.cpp
+++ rules/batiment/batimentquadmur.cpp
@@ -2,6 +2,7 @@
 
 BatimentQuadMur::BatimentQuadMur(Quad c, float height) : Chose(), c(c), height(height) {
 	addEntropy(c);
+	this->window = false;
 }
 
 void BatimentQuadMur::getBoundingBoxPoints() {
@@ -9,6 +10,25 @@ void BatimentQuadMur::getBoundingBoxPoints() {
 	addBBPoints(c + Vertex(0,0,height));
 }
 
+void BatimentQuadMur::setWindow(Quad w) {
+    this->windowPos = w;
+    this->window = true;
+}
+
+bool BatimentQuadMur::split() {
+    if(!window)
+        return false;
+
+    Quad left = c;
+    Quad right = c;
+    Quad top = c;
+    Quad bottom = c;
+
+
+    return true;
+}
+
 void BatimentQuadMur::triangulation() {
-    addGPUOcto(c, c + Vertex(0,0,height), 0xF1, 0xE0, 0xE0);
+    //addGPUOcto(c, c + Vertex(0,0,height), 0xF1, 0xE0, 0xE0);
+    addGPUOcto(c, c + Vertex(0,0,height), 0x60, 0x60, 0xFF);
 }
