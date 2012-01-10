ae0b4241a1ae2b0269da34284a9452e847daa00b
Yoann <yoann.b87@voila.fr>
2012-01-10 16:49:44 +0100

    Correction d'un bug sur les toits.

--- rules/batiment/batimentquadtoit.cpp
+++ rules/batiment/batimentquadtoit.cpp
@@ -13,7 +13,7 @@ void BatimentQuadToit::triangulation() {
     if(type == 1) {
         if(c.minLengthNS() < c.minLengthEW())
             c = c << 1;
-        c = c.inset(S, -20).inset(S,-20);
+        c = c.inset(N, -20).inset(S,-20);
         height = c.minLengthEW() / 5;
         Vertex ce = c[SE] + (c[NE] - c[SE])/2 + Vertex(0,0,height);
         Vertex cw = c[SW] + (c[NW] - c[SW])/2 + Vertex(0,0,height);
@@ -21,7 +21,7 @@ void BatimentQuadToit::triangulation() {
         addGPUTriangle(c[NW],cw,c[SW],0xF1,0xE0,0xE0);
         addGPUTriangle(c[SE],ce,c[NE],0xF1,0xE0,0xE0);
 
-        addGPUQuad(c,0xF1,0xE0,0xE0);
+        addGPUQuad(c[NE],c[NW],c[SW],c[SE],0xF1,0xE0,0xE0);
         addGPUQuad(c[NE],c[NW],cw,ce,0xE0,0x20,0x00);
         addGPUQuad(c[SW],c[SE],ce,cw,0xE0,0x20,0x00);
     }
