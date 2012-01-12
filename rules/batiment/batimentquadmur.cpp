5cd0ea382dd135b2716159c657a541fa0e355986
Yoann <yoann.b87@voila.fr>
2012-01-12 09:22:47 +0100

    Ok pour les fenêtres !

--- rules/batiment/batimentquadmur.cpp
+++ rules/batiment/batimentquadmur.cpp
@@ -15,23 +15,12 @@ void BatimentQuadMur::setWindow(bool val) {
     val = val;
     this->window = val;
     Quad q = Quad(ch[SE],c[SE],c[SW],ch[SW]);
-    int lr = (q.length(S) - 100)/2;
+    int lr = (q.length(S) - 120)/2;
 
-    Quad wFront = q.insetNESW(40,lr,110,lr);
-    Quad wBack = wFront.offsetNormal(280);
+    Quad wFront = q.insetNESW(90,lr,120,lr);
+    Quad wBack = wFront.offsetNormal(28);
     windowPos = Quad(wFront[SE],wBack[SE],wBack[SW],wFront[SW]);
     windowPosh = Quad(wFront[NE],wBack[NE],wBack[NW],wFront[NW]);
-
-    //addGPUQuad(wFront,0xFF,0xFF,0x00);
-    //addGPUQuad(wBack,0xFF,0xFF,0x00);
-    //addGPUQuad(windowPos,0x90,0x90,0xFF);
-    //addGPUQuad(windowPosh,0x90,0x90,0xFF);
-
-    //std::cout << "lr" << lr << std::endl;
-    //std::cout << windowPos[NE] << std::endl;
-    //std::cout << windowPos[SE] << std::endl;
-    //std::cout << q[SW] << std::endl;
-    //std::cout << q[NW] << std::endl << std::endl;
 }
 
 bool BatimentQuadMur::split() {
@@ -43,8 +32,7 @@ bool BatimentQuadMur::split() {
     Quad left = Quad(c[NE],c[SE],windowPos[SE],windowPos[NE]);
     Quad lefth = Quad(ch[NE],ch[SE],windowPosh[SE],windowPosh[NE]);
     Quad top = Quad(windowPosh[NE],windowPosh[NW],windowPosh[SW],windowPosh[SE]);
-//addGPUQuad(windowPosh[NE],top[NW],ch[NW],ch[NE],0xFF,0x90,0x90);
-//addGPUQuad(ch,0x80,0x80,0xFF);
+
     addChild(new BatimentQuadMur(c,windowPos));
     addChild(new BatimentQuadMur(windowPosh,ch));
     addChild(new BatimentQuadMur(left,lefth));
